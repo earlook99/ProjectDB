6b382af57fa6512993b39e0ffc12f23b588a983d
earlook99 <earlook99@gmail.com>
2026-05-11 11:53:58 +0900

    gitignore add

---
+++ Source/ProjectDB/ProjectDB.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectDB.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectDB, "ProjectDB" );
